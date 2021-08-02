7b30ef0fc6db5ec7291e10962c541d1f74fa1f78
lars3141 <86437205+lars3141@users.noreply.github.com>
2021-08-02 14:48:31 +0200

    Minor Fixes

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from experiment import runExperiment
 from multiprocessing import Pool
 from tqdm import tqdm
 import csv
+import warnings
+
+warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     for i in range(10):
+        intermediate = set()
         #set-up pools (number of Threads)
         p = Pool(8)
-        intermediate = tqdm(p.imap_unordered(runExperiment, [round(x * 0.01, 2) for x in range(101)]), total=101)
+        intermediate = intermediate.union(tqdm(p.imap_unordered(runExperiment, [round(x * 0.01, 2) for x in range(101)]), total=101))
         p.close()
         p.join()
-        print(f'Iteration {i + 1} finished.')
         with open('out.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
             for r in intermediate:
                 writer.writerow(r)
+        print(f'Iteration {i + 1} finished.')
